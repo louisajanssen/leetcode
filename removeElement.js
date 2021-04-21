99b6671ffac83f409703f8f35f148fa92cd94f33
louisajanssen <janssenlouisa1@gmail.com>
2021-04-21 16:01:53 -0700

    remove element

---
+++ removeElement.js
@@ -0,0 +1,17 @@
+var removeElement = function(nums, val) {
+    for(let i = nums.length - 1; i >= 0; i--){
+        if(nums[i] === val){
+            nums.splice(i,1)
+        }
+    }
+    return nums.length
+};
+
+let test1 = removeElement([2, 3, 3], 3)
+console.log(test1) // expect 1
+
+let test2 = removeElement([0,1,2,2,3,0,4,2], 2)
+console.log(test2) // expect 5
+
+let test3 = removeElement([3], 3)
+console.log(test3) // expect 0
