eb7b628f1cd3986a367a9bfeb71b12bdf38793e0
louisajanssen <janssenlouisa1@gmail.com>
2021-04-21 16:03:30 -0700

    remove element

--- removeElement.js
+++ removeElement.js
@@ -15,3 +15,9 @@ console.log(test2) // expect 5
 
 let test3 = removeElement([3], 3)
 console.log(test3) // expect 0
+
+let test4 = removeElement([3, 3, 3, 5, 7, 8, 9, 3], 3)
+console.log(test4) // expect 4
+
+let test5 = removeElement([2, 2, 2, 2, 2, 2], 2)
+console.log(test5) // expect 0
